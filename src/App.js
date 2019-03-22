3d1a9acdad43290f9450800e93ef48dbf2879636
ppenjolicy <yurin.pk.123@gmail.com>
2019-03-22 17:56:53 +0700

    lab6.2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Provider} from 'react-redux'
+import {createStore, combineReducers, applyMiddleware } from 'redux'
+import Bear from './Bear'
+import logger from 'redux-logger'
+import axios from 'axios'
+import thunk from 'redux-thunk'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+// ========  action (As Dispatcher) ==========
+export const add = () => ({type: 'ADD'})
+export const add2 = (number) => ({type: 'ADD2', number})
+export const minus = () => ({ type: 'MINUS'})
+
+export const getBearsSuccess = bears => ({
+   type: 'GET_BEARS_SUCCESS',
+   bears
+});
+export const getBearsFailed = () => ({ type: 'GET_BEARS_FAILED'});
+
+export const getBears = (name) => async (dispatch) => {
+   try {
+       console.log('get bear new')
+       const response = await axios.get(`https://api.github.com/users/${name}`)
+       const responseBody = await response.data;
+       console.log('response: ', responseBody)
+       dispatch(getBearsSuccess(responseBody));
+   } catch (error) {
+       console.error(error);
+       dispatch(getBearsFailed());
+   }
 }
 
-export default App;
+
+// ========  reducer (As Controller) =========
+export const numberReducer =  (state = 0, action) => {
+   switch (action.type) {
+       case 'ADD':
+           console.log('my add...')
+           return state + 1
+       case 'MINUS':
+           return state - 1
+       case 'ADD2':
+           return state + action.number
+       default:
+           return state
+   }
+}
+
+export const bearReducer = (state = 0, action) => {
+   switch (action.type) {
+       case 'GET_BEARS_SUCCESS':
+               console.log('action: ' , action.bears)
+           return action.bears
+       case 'GET_BEARS_FAILED':
+               console.log('action: Failed')
+           return action.bears
+       default:
+           return state
+   }
+}
+
+
+export const rootReducer = combineReducers({number: numberReducer, bears: bearReducer})
+export const store = createStore(rootReducer, applyMiddleware(logger,thunk))
+
+
+// ======== wrap root element by Provider with Store ========
+class App extends Component {
+   render() {
+       return <Provider store={store}><Bear/></Provider>
+   }
+}
+
+export default App
